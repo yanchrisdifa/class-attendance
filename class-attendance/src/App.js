806803ee41c1695f39daa64a1595d849c4d072b0
zettacamp-YanChris-Difa <chris.difa@zettacamp.pro>
2022-07-15 02:32:22 +0800

    - Add sweet alert if the user has more than one role - Add routing to dashboard page

--- class-attendance/src/App.js
+++ class-attendance/src/App.js
@@ -1,8 +1,22 @@
 import "./App.scss";
 import Login from "./login/login";
+import Dashboard from "./dashboard/dashboard";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 
 function App() {
-  return <Login />;
+  return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login/>}></Route>
+          <Route path="/dashboard" element={<Dashboard/>}></Route>
+        </Routes>
+      </Router>
+    );
 }
 
 export default App;
