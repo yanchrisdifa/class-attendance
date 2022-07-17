46bfe0c7db89ec97a48e4dac5e5288103105a38f
zettacamp-YanChris-Difa <chris.difa@zettacamp.pro>
2022-07-18 07:03:26 +0800

    List of Changes : - fixing routing to display menus inside sidebar content

--- class-attendance/src/sidebar/sidebar.js
+++ class-attendance/src/sidebar/sidebar.js
@@ -1,20 +1,14 @@
 import "./sidebar.scss";
 import { BsTable } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
-import { useParams, useNavigate, useLocation } from "react-router-dom";
-import Dashboard from "../dashboard/dashboard";
-import Login from "../login/login";
-import Table from "../table/table";
+import { Link, Outlet } from "react-router-dom";
+import React from "react";
 
-const menus = require('../data/sidebar-menus.json')
+const menus = require('../data/sidebar-menus.json');
 
 const Sidebar = () => {
-    const navigate = useNavigate()
-    const activeMenuName = useParams()
-    const location = useLocation()
-    console.log('user active ', location.state.userActive)
     return (
         <div className="sidebar">
+            <div className="sidebar-content-container">
             <div className="sidebar-container">
                 <div className="user-profile-container">
                     <div className="profile-picture-container"></div>
@@ -24,25 +18,27 @@ const Sidebar = () => {
                     </div>
                 </div>
                 <div className="menus-container">
-                    <Link to="/">
                         {
                             menus.map((menu, index) => {
                                 return <div className={`menu menu-${index + 1}`} key={index}>
-                                    <BsTable/>
-                                    <div className="label-container">
-                                    {menu.label}
-                                    </div>
+                                    <Link to={menu.route} className={`link link-${index + 1}`}>
+                                        <BsTable/>
+                                        <div className="label-container">
+                                        {menu.label}
+                                        </div>
+                                    </Link>
                                 </div>
                             })
                         }
-                    </Link>
                 </div>
             </div>
+            </div>
             <div className="sidebar-content">
-                <Table/>
+                <Outlet/>
             </div>
         </div>
     )
 }
+
 
 export default Sidebar
